1d52aa847c60c0c894b78239568cff197b20b7a5
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-02 17:49:41 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ async function getData() {
     console.log(products)
     products.forEach(element => {
         if(element.id <= 4){
+
             const card = document.createRange().createContextualFragment(`
                 
                 <div class="card">
@@ -33,4 +34,53 @@ async function getData() {
     });
 }
 
+const btn_validar = document.getElementById('btn-validar')
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const messageArr = ["nombre", "email", "mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i++){
+        for(i = 0; i < messageArr.length; i++){
+            if(arr[i].value == ""){
+                swal({
+                    title: `El campo ${messageArr[i]} no puede estar vacío`,
+                    icon: "error",
+                 })
+                 return false;
+            }
+        }
+        if(!emailvalido(email.value)){
+            swal({
+                title: `El formato del ${messageArr[1]} no es correcto`,
+                icon: "error",
+             })
+             return false;
+        }
+    }
+    nombre.value = "";
+    email.value = "";
+    mensaje.value = "";
+    return true;
+
+}
+
+const mensaje  = ()=>{
+    Swal.fire({
+        icon: "info",
+        title: "Atención...",
+        text: "Será redirigido a Google Maps",
+        showConfirmButton: false,
+        timer: 3000,
+      }); 
+      window.location.href = mensaje  
+}
+
+const emailvalido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+btn_validar.addEventListener("click", validar)
 getData()
